d0dff43ebf2ab8ddfc1244920f1f7d65730c18c2
anton-ua <antonyua23@gmail.com>
2020-01-17 00:02:51 +0200

    hw-08

---
+++ js/script.js
@@ -0,0 +1,54 @@
+import images from "./gallery-items.js";
+
+const links = {
+  gallery: document.querySelector(".js-gallery"),
+  lightbox: document.querySelector(".js-lightbox"),
+  lightboxImage: document.querySelector(".lightbox__image"),
+  closeLightbox: document.querySelector(
+    ".lightbox__button[data-action=close-lightbox]"
+  ),
+  lightboxOverlay: document.querySelector(".lightbox__overlay")
+};
+
+createGallery(images);
+
+links.gallery.addEventListener("click", openLightbox);
+
+links.closeLightbox.addEventListener("click", closeLightbox);
+
+function createGallery(images) {
+  images.map(el => {
+    links.gallery.insertAdjacentHTML(
+      "beforeend",
+      `<li class="gallery__item">
+      <a
+        class="gallery__link"
+        href=${el.original}>
+        <img
+          class="gallery__image"
+          src=${el.preview}
+          data-source=${el.original}
+          alt="${el.description}"
+        />
+      </a>
+    </li>`
+    );
+  });
+}
+
+function openLightbox(event) {
+  if (event.target.nodeName === "IMG") {
+    event.preventDefault();
+    links.lightbox.classList.add("is-open");
+    links.lightboxImage.attributes.src.value =
+      event.target.attributes["data-source"].value;
+    links.lightboxImage.attributes.alt.value =
+      event.target.attributes.alt.value;
+  }
+}
+
+function closeLightbox(event) {
+  links.lightbox.classList.remove("is-open");
+  links.lightboxImage.attributes.src.value = "";
+  links.lightboxImage.attributes.alt.value = "";
+}
